509dab4a3e3b82c515f4c14fe48dedd0698e05dc
Damon Liu <pairwinter@126.com>
2014-08-06 17:22:11 +0800

    update

--- tasks/tiny_i18n.js
+++ tasks/tiny_i18n.js
@@ -7,44 +7,62 @@
  */
 
 'use strict';
+var path = require('path');
+var _ = require('underscore');
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
+    function unicode(str){
+        var newStr = str.replace(/[^\u0000-\u007f]/ig,function(s){
+            return "\\u"+(parseInt(s.charCodeAt(0))).toString(16);
+        });
+        return newStr;
+    }
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('tiny_i18n', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    grunt.registerMultiTask('tiny_i18n', 'The best Grunt plugin ever.', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            var i18nFiles = f.i18n;
+            var i18nJsons = {};
+            i18nFiles.forEach(function(i18nFilePath){
+                var i18nName = path.basename(i18nFilePath, '.json');
+                var json = grunt.file.readJSON(i18nFilePath);
+                for(var key in json){
+                    if(json.hasOwnProperty(key)){
+                        json[key] = unicode(json[key])
+                    }
+                }
+                i18nJsons[i18nName] = json;
+            });
+            // Concat specified files.
+            var srcFiles = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+            srcFiles.forEach(function (filepath) {
+                // Read file source.
+                var fileContent =  grunt.file.read(filepath);
+                _.forEach(i18nJsons,function(i18nJson,i18nName){
+                    var resuleContent = grunt.template.process(fileContent,{data:i18nJson});
+                    grunt.file.write(f.dest+'/'+i18nName+'/'+filepath, resuleContent);
+                });
+            });
+
+            // Print a success message.
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
 
 };
